c536c1194d9ff62e3c905832e37972492a07bd4a
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-08-29 10:39:46 +0530

    remarkable, an external Markdown library to create a text-area app

--- src/App.js
+++ src/App.js
@@ -1,68 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
+import Remarkable from 'remarkable';
 import './App.css';
 
-class TodoApp extends React.Component {
+class MarkdownEditor extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { items: [], text: '' };
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  render() {
-    return (
-      <div>
-        <h3>TODO</h3>
-        <TodoList items={this.state.items} />
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="new-todo">
-            What needs to be done?
-          </label>
-          <input
-            id="new-todo"
-            onChange={this.handleChange}
-            value={this.state.text}
-          />
-          <button>
-            Add #{this.state.items.length + 1}
-          </button>
-        </form>
-      </div>
-    );
+    this.state = { value: 'Hello, **world**!' };
   }
 
   handleChange(e) {
-    this.setState({ text: e.target.value });
+    this.setState({ value: e.target.value });
   }
 
-  handleSubmit(e) {
-    e.preventDefault();
-    if (!this.state.text.length) {
-      return;
-    }
-    const newItem = {
-      text: this.state.text,
-      id: Date.now()
-    };
-    this.setState(state => ({
-      items: state.items.concat(newItem),
-      text: ''
-    }));
+  getRawMarkup() {
+    const md = new Remarkable();
+    return { __html: md.render(this.state.value) };
   }
-}
 
-class TodoList extends React.Component {
   render() {
     return (
-      <ul>
-        {this.props.items.map(item => (
-          <li key={item.id}>{item.text}</li>
-        ))}
-      </ul>
+      <div className="MarkdownEditor">
+        <h3>Input</h3>
+        <label htmlFor="markdown-content">
+          Enter some markdown
+        </label>
+        <textarea
+          id="markdown-content"
+          onChange={this.handleChange}
+          defaultValue={this.state.value}
+        />
+        <h3>Output</h3>
+        <div
+          className="content"
+          dangerouslySetInnerHTML={this.getRawMarkup()}
+        />
+      </div>
     );
   }
 }
 
 
-export default TodoApp;
+export default MarkdownEditor;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import TodoApp from './App';
+import MarkdownEditor from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<TodoApp />, document.getElementById('root'));
+ReactDOM.render(<MarkdownEditor />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
